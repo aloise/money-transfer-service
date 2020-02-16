45b5c4f880a369385e35fc71d6054d7c769b3fbf
Ihor <erwaht@gmail.com>
2020-02-16 22:51:24 +0100

    transaction service - updated tests

--- src/test/kotlin/TransactionServiceTest.kt
+++ src/test/kotlin/TransactionServiceTest.kt
@@ -4,6 +4,7 @@ import name.aloise.service.*
 import kotlin.test.Test
 import kotlin.test.assertEquals
 import kotlin.test.assertNotNull
+import kotlin.test.assertNull
 
 @ExperimentalCoroutinesApi
 class TransactionServiceTest {
@@ -13,7 +14,7 @@ class TransactionServiceTest {
     }
 
     @Test
-    fun testCreateAccount() = runBlockingTest {
+    fun testTransferFunds() = runBlockingTest {
         val (transactions, accounts) = getServices()
 
         val (acc1, _) = accounts.create(CreateAccount("test", 1000))
@@ -33,6 +34,30 @@ class TransactionServiceTest {
         assertEquals(1500, updatedAcc2.centAmount)
         assertEquals(trx.id, updatedAcc1.lastTransactionId)
         assertEquals(trx.id, updatedAcc2.lastTransactionId)
+
+    }
+
+    @Test
+    fun testFailToTransferInsufficientFunds() = runBlockingTest {
+        val (transactions, accounts) = getServices()
+
+        val (acc1, _) = accounts.create(CreateAccount("test3", 1000))
+        val (acc2, _) = accounts.create(CreateAccount("test4", 1000))
+
+        val trx = transactions.create(acc1.id, acc2.id, 2500)
+
+        assertEquals(2500, trx.centAmount)
+        assertEquals(acc1.id, trx.fromAccountId)
+        assertEquals(acc2.id, trx.toAccountId)
+        assertEquals(TransactionStatus.FAILED, trx.status)
+
+        val (_, updatedAcc1) = assertNotNull(accounts.get(acc1.id))
+        val (_, updatedAcc2) = assertNotNull(accounts.get(acc2.id))
+
+        assertEquals(1000, updatedAcc1.centAmount)
+        assertEquals(1000, updatedAcc2.centAmount)
+        assertNull(updatedAcc1.lastTransactionId)
+        assertNull(updatedAcc2.lastTransactionId)
 
     }
 
